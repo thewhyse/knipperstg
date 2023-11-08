d071fd7b28a489d4e050427d044e401d73a00f8e
billiemead <bmead@mjhlifesciences.com>
2023-11-08 16:20:05 -0500

    Pencil banner - final pre-deploy live build

--- wp-content/themes/knipper/dist/js/bundle.js
+++ wp-content/themes/knipper/dist/js/bundle.js
@@ -1,13 +1,1 @@
-/******/ (() => { // webpackBootstrap
-var __webpack_exports__ = {};
-/*!**************************!*\
-  !*** ./src/js/bundle.js ***!
-  \**************************/
-jQuery(document).ready(function ($) {
-  $('.map-ping, #close-map-icon').on('click', function () {
-    $('#map_popup_section_2023').toggle();
-  });
-});
-/******/ })()
-;
-//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiYnVuZGxlLmpzIiwibWFwcGluZ3MiOiI7Ozs7O0FBQUFBLE1BQU0sQ0FBQ0MsUUFBUSxDQUFDLENBQUNDLEtBQUssQ0FBQyxVQUFVQyxDQUFDLEVBQUU7RUFFaENBLENBQUMsQ0FBQyw0QkFBNEIsQ0FBQyxDQUFDQyxFQUFFLENBQUMsT0FBTyxFQUFFLFlBQVk7SUFDcERELENBQUMsQ0FBQyx5QkFBeUIsQ0FBQyxDQUFDRSxNQUFNLENBQUMsQ0FBQztFQUN6QyxDQUFDLENBQUM7QUFFTixDQUFDLENBQUMsQyIsInNvdXJjZXMiOlsid2VicGFjazovL2tuaXBwZXIvLi9zcmMvanMvYnVuZGxlLmpzIl0sInNvdXJjZXNDb250ZW50IjpbImpRdWVyeShkb2N1bWVudCkucmVhZHkoZnVuY3Rpb24gKCQpIHtcclxuXHJcbiAgICAkKCcubWFwLXBpbmcsICNjbG9zZS1tYXAtaWNvbicpLm9uKCdjbGljaycsIGZ1bmN0aW9uICgpIHtcclxuICAgICAgICAkKCcjbWFwX3BvcHVwX3NlY3Rpb25fMjAyMycpLnRvZ2dsZSgpO1xyXG4gICAgfSk7XHJcblxyXG59KTsiXSwibmFtZXMiOlsialF1ZXJ5IiwiZG9jdW1lbnQiLCJyZWFkeSIsIiQiLCJvbiIsInRvZ2dsZSJdLCJzb3VyY2VSb290IjoiIn0=
+jQuery(document).ready((function(n){n(".map-ping, #close-map-icon").on("click",(function(){n("#map_popup_section_2023").toggle()}))}));
